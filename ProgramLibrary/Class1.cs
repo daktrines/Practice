99dd8b8b7a6dc229fe658c44a26a72c9d8e2512b
daktrines <ktkln2003@gmail.com>
2022-01-16 15:01:10 +0300

    Добавьте файлы проекта.

---
+++ ProgramLibrary/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramLibrary
+{
+    public class Class1
+    {
+        /// <summary>
+        /// Решение 4-х заданий по учебной практике
+        /// Расчет 1 задания: Ввести трехзначное число. Определить: верно ли, что все его цифры одинаковые?
+        /// </summary>
+        /// <param name="value"> трехзначное число </param>
+        /// <returns></returns>
+        public static bool Calculate1(int value)
+        {
+            bool da;
+            int value1 = value % 10;
+            value = value / 10;
+            int value2 = value % 10;
+            value = value / 10;
+            int value3 = value % 10;
+            if (value1 == value2 && value1 == value3) da = true;
+            else da = false;
+            return da;
+        }
+
+        /// <summary>
+        /// Расчет 2 задания: Ввести три целых числа. Найти количество положительных и отрицательных чисел
+        /// </summary>
+        /// <param name="value1"> первое натуральное число </param>
+        /// <param name="value2"> второе натуральное число </param>
+        /// <param name="value3"> третье натуральное число </param>
+        /// <param name="kol1"> количество положительных чисел </param>
+        /// <param name="kol2"> количество отрицательных чисел </param>
+        public static void Calculate2(int value1, int value2, int value3, out int kol1, out int kol2)
+        {
+            kol1 = 0; kol2 = 0;
+            if (value1 > 0) kol1++;
+            else kol2++;
+            if (value2 > 0) kol1++;
+            else kol2++;
+            if (value3 > 0) kol1++;
+            else kol2++;
+        }
+    }
+}
---
+++ УП/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ProgramLibrary;
+
+namespace УП
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void CalculationOfTheFirstNumber_Click(object sender, RoutedEventArgs e)
+        {
+            int value = Convert.ToInt32(Value.Text);
+            bool da = Class1.Calculate1(value);
+            if (da == true) Rez.Text = "Верно (Все цифры одинаковы)";
+            else Rez.Text = "Неверно (Цифры не одинаковы)";
+        }
+
+        private void CalculationOfTheSecondNumber_Click(object sender, RoutedEventArgs e)
+        {
+            int value1 = Convert.ToInt32(Value1.Text);
+            int value2 = Convert.ToInt32(Value2.Text);
+            int value3 = Convert.ToInt32(Value3.Text);
+            Class1.Calculate2(value1, value2, value3, out int kol1, out int kol2);
+            Kol1.Text = kol1.ToString();
+            Kol2.Text = kol2.ToString();
+        }
+        private void CalculationOfTheThirdNumber_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CalculationOfTheFourthNumber_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
